5a259d6907e1b022e9af0fedc755a4ff061a2fbc
Tern321 <55168319+Tern321@users.noreply.github.com>
2020-12-10 09:22:40 +0300

    bug fix

--- js/Network.js
+++ js/Network.js
@@ -50,7 +50,7 @@ class Network {
         return Network.generateWriteUrl(login, "file", "notepadDataUpdateTime", UpdateDataRequestController.lastChangeTime);
     }
 }
-Network.localhosted = true;
+Network.localhosted = false;
 class PostRequestData {
 }
 //# sourceMappingURL=Network.js.map
